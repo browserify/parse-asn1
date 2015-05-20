be4e43d8ca8a766ef77caf724e7204b3e299d601
Daniel Cousens <dcousens@users.noreply.github.com>
2015-05-20 22:53:11 +1000

    evp: standard-ish formatting

--- EVP_BytesToKey.js
+++ EVP_BytesToKey.js
@@ -1,45 +1,45 @@
 var createHash = require('create-hash');
 
-module.exports = function evp(password, salt, keyLen) {
-  keyLen = keyLen / 8;
+module.exports = function evp (password, salt, keyLen) {
+  keyLen = keyLen / 8
   
-  var ki = 0;
-  var key = new Buffer(keyLen);
-  var addmd = 0;
-  var md, md_buf;
-  var i;
+  var ki = 0
+  var key = new Buffer(keyLen)
+  var addmd = 0
+  var md, md_buf, i
   
   while (keyLen > 0) {
-    md = createHash('md5');
+    md = createHash('md5')
     
     if (addmd > 0) {
-       md.update(md_buf);
+       md.update(md_buf)
     }
     
     addmd++
     
-    md.update(password);
-    md.update(salt);
-    md_buf = md.digest();
+    md.update(password)
+    md.update(salt)
+    md_buf = md.digest()
     
-    i = 0;
+    i = 0
     
     while (keyLen > 0) {
       if (i === md_buf.length) {
-        break;
+        break
       }
       
-      key[ki] = md_buf[i];
+      key[ki] = md_buf[i]
       
-      ki++;
-      i++;
-      keyLen--;
+      ki++
+      i++
+      keyLen--
     }
   }
   
+  // zero the md_buf
   for (i = 0; i < md_buf.length; i++) {
-    md_buf[i] = 0;
+    md_buf[i] = 0
   }
   
-  return key;
-};
+  return key
+}
