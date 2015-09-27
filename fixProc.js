b0e3b8df589fcaef917c8ac5e509c8a92feaeac9
Calvin Metcalf <calvin.metcalf@gmail.com>
2015-09-27 10:44:52 -0400

    pull out evp

--- fixProc.js
+++ fixProc.js
@@ -2,7 +2,7 @@
 var findProc = /Proc-Type: 4,ENCRYPTED\r?\nDEK-Info: AES-((?:128)|(?:192)|(?:256))-CBC,([0-9A-H]+)\r?\n\r?\n([0-9A-z\n\r\+\/\=]+)\r?\n/m;
 var startRegex =/^-----BEGIN (.*) KEY-----\r?\n/m;
 var fullRegex = /^-----BEGIN (.*) KEY-----\r?\n([0-9A-z\n\r\+\/\=]+)\r?\n-----END \1 KEY-----$/m;
-var evp = require('./EVP_BytesToKey');
+var evp = require('evp_bytestokey');
 var ciphers = require('browserify-aes');
 module.exports = function (okey, password) {
   var key = okey.toString();
@@ -15,7 +15,7 @@ module.exports = function (okey, password) {
     var suite = 'aes' + match[1];
     var iv = new Buffer(match[2], 'hex');
     var cipherText = new Buffer(match[3].replace(/\r?\n/g, ''), 'base64');
-    var cipherKey = evp(password, iv.slice(0,8), parseInt(match[1]));
+    var cipherKey = evp(password, iv.slice(0,8), parseInt(match[1])).key;
     var out = [];
     var cipher = ciphers.createDecipheriv(suite, cipherKey, iv);
     out.push(cipher.update(cipherText));
